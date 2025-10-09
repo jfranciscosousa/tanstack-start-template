c254560edf984588ae3d06dcf7b4203180316a93
Francisco Sousa <francisco.sousa@hey.com>
2025-10-09 14:43:49 +0100

    Fix vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import checker from "vite-plugin-checker";
-import react from "@vitejs/plugin-react"
+import react from "@vitejs/plugin-react";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
 export default defineConfig({
   server: {
@@ -17,6 +18,10 @@ export default defineConfig({
     }),
 
     tanstackStart(),
+
+    nitroV2Plugin({
+      preset: "vercel",
+    }),
 
     react(),
 
