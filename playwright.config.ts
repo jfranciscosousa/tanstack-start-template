2ac1890fa5ec036b7fdfe76008fc5e33a7de2c73
Francisco Sousa <francisco.sousa@hey.com>
2025-10-15 19:45:31 +0100

    Reduce timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,8 @@ const config: PlaywrightTestConfig = {
   testDir: "./src/test/e2e",
   webServer: {
     command: "PORT=3001 bin/dev",
-    url: 'http://localhost:3001',
+    url: "http://localhost:3001",
+    timeout: 5000,
   },
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
@@ -26,7 +27,7 @@ const config: PlaywrightTestConfig = {
   /* We are cleaning the database with each test so make sure we only have 1 worker to avoid cleaning the database in the middle of a parallel test */
   workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
