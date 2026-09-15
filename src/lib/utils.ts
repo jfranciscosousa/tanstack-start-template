f44b4785f28bbbaf830e135ad9d69b9b1f111e75
Francisco Sousa <francisco.sousa@hey.com>
2026-09-16 00:29:40 +0100

    chore: migrate shadcn cn utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,1 @@
-import type { ClassValue } from "clsx";
-
-import { twMerge } from "tailwind-merge";
-import { clsx } from "clsx";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+export { cn } from "cn";
